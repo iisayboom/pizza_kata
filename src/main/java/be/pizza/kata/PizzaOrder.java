19b860f2fbca3c90f47ab7efd15dff6a446aa230
Niels De Bruyne <niels.debruyne@dxc.com>
2025-05-28 09:36:02 +0200

    Fix columns not being recognised in PizzaOrder

--- src/main/java/be/pizza/kata/PizzaOrder.java
+++ src/main/java/be/pizza/kata/PizzaOrder.java
@@ -1,15 +1,13 @@
 package be.pizza.kata;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Entity
 @Data
+@Table(name = "pizza_order")
 public class PizzaOrder {
 
     @Id
